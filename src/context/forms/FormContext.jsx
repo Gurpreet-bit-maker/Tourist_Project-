d1dda659981d878d4c9b7bff5c41036d61994cc0
Gurpreet <ajbagdi6@gmail.com>
2026-01-25 05:44:32 +0530

    closesignup api

--- src/context/forms/FormContext.jsx
+++ src/context/forms/FormContext.jsx
@@ -4,17 +4,17 @@ import userFormContext from "./formcreate";
 
 let ShareFormContext = ({ children }) => {
   let [signUpData, setSignUpData] = useState([]);
-  useEffect(() => {
-    let call = async () => {
-      try {
-        let signDb = await axios.get("http://localhost:8080/signUp/userData");
-        setSignUpData(signDb.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    call();
-  }, []);
+  // useEffect(() => {
+  //   let call = async () => {
+  //     try {
+  //       let signDb = await axios.get("http://localhost:8080/signUp/userData");
+  //       setSignUpData(signDb.data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   call();
+  // }, []);
 
   return (
     <userFormContext.Provider value={{  }}>
